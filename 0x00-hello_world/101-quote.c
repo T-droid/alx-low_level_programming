e1fcd4a9f9cf9d979727ccdf273395a89f7d0e70
emmanuel tindi <emmanueltindi66@gmail.com>
2023-03-17 09:58:49 -0700

    update file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,10 +8,9 @@
 
 int main(void)
 {
-	const char *message = "and that piece of art is useful\n" +
-		"-Dora Korpar, 2015-10-19\n";
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(STDERR_FILENO, message, 54);
+		write(STDERR_FILENO, message, 54);
 
 	return (1);
 }
