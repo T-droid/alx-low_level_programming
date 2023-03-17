8f2787bc469eec7feb731685fec39fa6204dc2eb
emmanuel tindi <emmanueltindi66@gmail.com>
2023-03-17 04:37:24 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -function for the main
+ *
+ * printf: prints a string
+ *
+ * return: always returns 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
