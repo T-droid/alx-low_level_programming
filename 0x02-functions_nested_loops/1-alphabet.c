ea1559701eb95093d0af608fcdd7e7ebdbb71e72
emmanuel tindi <emmanueltindi66@gmail.com>
2023-03-21 11:14:44 -0700

    lower case alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lower case
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
