73a3bac4dae572c47c42283b6bf008877aaa575d
emmanuel tindi <emmanueltindi66@gmail.com>
2023-03-22 09:51:06 -0700

    lern your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+	int row, col, prod;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			prod = row * col;
+			if (col == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				_putchar(' ');
+				if (prod <= 9)
+				{
+					_putchar(' ');
+				}
+			}
+			if (prod == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				_putchar(prod / 10 + '0');
+				_putchar(prod % 10 + '0');
+			}
+			if (col < 9)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
