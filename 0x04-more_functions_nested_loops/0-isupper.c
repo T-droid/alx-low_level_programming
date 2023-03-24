63342551827d70ad8105ebacf4e3811e88e3401b
emmanuel tindi <emmanueltindi66@gmail.com>
2023-03-24 06:26:33 -0700

    uppercases

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase
+ * _isupper - checks for cases
  *
- * @: parameter to checked
- * Return: returns 1 if uppercase and 0 if lowercase
+ * @c: parameter to be checked
+ * Return: returns 1 if upper
+ * and 0 if lower
  */
+
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
 	else
 		return (0);
 }
