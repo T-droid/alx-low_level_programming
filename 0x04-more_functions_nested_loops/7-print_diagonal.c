3348f60c5ef2cfa02ffe6c55aa8b9348d4ae702f
emmanuel tindi <emmanueltindi66@gmail.com>
2023-03-24 19:17:00 -0700

    diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,12 @@
  */
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-
-	int i;
-	int j;
-
 	for (i = 0 ; i < n ; i++)
 	{
 		for (j = 0 ; j < i ; j++)
@@ -23,10 +21,6 @@ void print_diagonal(int n)
 			_putchar(' ');
 		}
 		_putchar('\\');
-		for (j = 0 ; j < n - i - 1 ; j++)
-		{
-			_putchar(' ');
-		}
 		_putchar('\n');
 	}
 }
