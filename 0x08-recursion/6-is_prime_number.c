0c42bb72a3fbe62eaf7ff8bd073003f2c262a289
emmanuel tindi <emmanueltindi66@gmail.com>
2023-04-05 12:06:45 -0700

    inception is possible

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,23 @@
 #include "main.h"
+#include <math.h>
+
+/**
+ * is_prime_number_helper - checks if a number is prime recursively
+ * @n: the number to be checked
+ * @i: the current divisor being tested
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number_helper(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+	if (i == 1)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (is_prime_number_helper(n, i - 1));
+}
 
 /**
  * is_prime_number - returns i if is an prime number
@@ -9,7 +28,5 @@
  */
 int is_prime_number(int n)
 {
-	if (n < = 1)
-		return (0);
-	else
-		return (is_prime_
+	return (is_prime_number_helper(n, (int) sqrt(n)));
+}
