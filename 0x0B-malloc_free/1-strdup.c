bdff6fa64c0b4c46621405974c94be81e88f03e2
emmanuel tindi <emmanueltindi66@gmail.com>
2023-04-11 12:43:20 -0700

    array of arrays

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns pointer to a newly allocated space
+ *
+ * @str: string to be returned
+ * Return: returns to apointer
+ */
+char *_strdup(char *str)
+{
+
