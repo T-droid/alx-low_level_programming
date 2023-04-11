decf5b022deeb7a653bb7c8cab2a85738c6de5be
emmanuel tindi <emmanueltindi66@gmail.com>
2023-04-11 13:39:30 -0700

    hard maloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns pointer to two dimensional array
+ *
+ * @width: parameter
+ * @height: parameter
+ * Return: returns NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = calloc(width, sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+	return (grid);
+}
