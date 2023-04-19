46342844ea8a180bcc65634c49600f82294a3079
emmanuel tindi <emmanueltindi66@gmail.com>
2023-04-19 11:41:09 -0700

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,24 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, num;
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j <= 14 ; j++)
 		{
-			_putchar(j + '0');
+			num = i * 15 + j;
+			if (num < 100)
+			{
+				_putchar(num / 10 + '0');
+				_putchar(num % 10 + '0');
+			}
+			else
+			{
+				_putchar('1');
+				_putchar(num % 10 + '0');
+			}
+			_putchar(j == 14 ? '\n' : ' ');
 		}
-		_putchar('\n');
 	}
 }
--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,13 +7,19 @@
  * Return: return 1 if string is palindrome
  * And 0 if otherwise
  */
-int is_palindrome(char *s);
-
-int is_palindrome_helper(char *s, int start, int end)
+int is_palindrome(char *s)
 {
-	if (start >= end)
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (s[start] != s[end])
-		return (0);
-	return (is_palindrome_helper(s, start + 1, end - 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+			return (1);
+		if (*s1 == '\0')
+			return (wildcmp(s1, s2 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1 + 1));
+	}
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
