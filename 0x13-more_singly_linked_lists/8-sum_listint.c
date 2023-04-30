3c892fa2f47e36277d1e7d4a49bcfc6c36a9c650
emmanuel tindi <emmanueltindi66@gmail.com>
2023-04-30 02:17:58 -0700

    summing

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sums up the list
+ * @head: head pointer
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
