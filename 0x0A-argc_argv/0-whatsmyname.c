eff2405cef219083001f6f2f983f6c993f448fa5
Emmanuel Tindi <emmanueltindi81@gmail.com>
2023-06-09 01:06:42 -0700

    function to print the executable name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argv: array of commandlind arguments
+ * @argc: number of the arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
