fc6de3bf8ba294bd40c3d47ac798c95954989e7f
Emmanuel Tindi <emmanueltindi81@gmail.com>
2023-06-12 05:38:56 -0700

    implimenting the strcat function

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string to be concatenated to
+ * @s2: string to concatenate
+ * Return: pointer to the concanated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int i, j, count = 0;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		count++;
+		j++;
+	}
+
+	ptr = (char *)malloc(sizeof(char) * count + 1);
+	if (ptr == NULL)
+		return (NULL);
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		ptr[i++] = s2[j];
+		j++;
+	}
+
+	ptr[i] = '\0';
+
+	return (ptr);
+}
