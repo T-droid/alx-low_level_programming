d0d3d1f4d5012343d00ca7fd9a595ea6d2c1a160
Emmanuel Tindi <emmanueltindi66@gmail.com>
2023-06-30 05:28:32 -0700

    file updste

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,18 +3,8 @@
 /**
  * myfunct - executes before the main
  */
-void myfunc(void) __attribute__ ((constructor));
+void myfunc(void)__attribute__((constructor));
+void myfunc(void)
 {
 	printf("You're beat! and yet, you must allow, \nI bore my house upon my back!\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
 }
