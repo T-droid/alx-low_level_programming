5ad7241d1ca1f5af268194da351abc343cf0a15e
Emmanuel Tindi <emmanueltindi66@gmail.com>
2023-06-30 06:46:22 -0700

    assembly language

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- * myfunct - executes before the main
+ * print_message - prints message before main
+ * Return: nothing
  */
-void myfunc(void)__attribute__((constructor));
-void myfunc(void)
+void print_message(void) __attribute__((constructor));
+
+void print_message(void)
 {
-	printf("You're beat! and yet, you must allow, \nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
