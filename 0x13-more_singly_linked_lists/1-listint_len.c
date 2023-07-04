cbb21af47ea72c161ab29b4ff6cf195d846654ba
Emmanuel Tindi <emmanueltindi66@gmail.com>
2023-07-03 22:49:52 -0700

    function to count the number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - count the number of elements in alist
+ * @h: head pointer
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
