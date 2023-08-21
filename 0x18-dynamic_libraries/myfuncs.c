40da116860b350cd36cad8580120bca2ff1599ec
Emmanuel Tindi <emmanueltindi66@gmail.com>
2023-08-21 16:56:39 +0300

    cytpes

---
+++ 0x18-dynamic_libraries/myfuncs.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * add - sums two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multipliers two integer
+ * @a: first integer
+ * @b: second integer
+ * Return: product of two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * mod - finds the modulus of two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: the modulus
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the quotient of two nos
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
