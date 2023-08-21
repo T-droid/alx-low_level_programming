cb95077d6e3b5cbb51945751ee6ff3ca1c699eaf
Emmanuel Tindi <emmanueltindi66@gmail.com>
2023-08-21 21:28:28 +0300

    code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-gcc -c -fPIC test.c
-gcc -shared -o libexample.so test.o
+gcc -g -fPIC -Wall -Werror -Wextra -pedantic test.c -shared -o libexample.so
 LD_PRELOAD=/$PWD/libexample.so ./gm 9 8 10 24 75 9
